ee5ea8afa8b708e9d724bcd5a5eb20fe1446c2f7
Georgsiedel <georgsiedel@gmx.de>
2025-03-25 15:45:02 +0100

    comparison reset

--- attack_comparison.py
+++ attack_comparison.py
@@ -47,11 +47,11 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', type=str, default='imagenet', choices=['cifar10', 'imagenet'],
                         help="Dataset to use")
     parser.add_argument('--samplesize_accuracy', type=int, default=10000, help="Split size for test accuracy evaluation")
-    parser.add_argument('--samplesize_attack', type=int, default=1, help="Split size for attack evaluation")
+    parser.add_argument('--samplesize_attack', type=int, default=1000, help="Split size for attack evaluation")
     parser.add_argument('--validation_run', type=utils.str2bool, nargs='?', const=False, default=False, 
                         help="True for validation/tuning, False for testing. Selects attackset from the front or the back")
     parser.add_argument('--dataset_root', type=str, default='../data', help="data folder relative root")
-    parser.add_argument('--model', type=str, default='DVCE_R50',
+    parser.add_argument('--model', type=str, default='vgg19',
                         help="Model name (e.g., standard,ViT_revisiting,ConvNext_iso_CvSt_revisiting,Salman2020Do_R50,corruption_robust,MainiAVG...)")
     parser.add_argument('--model_norm', type=str, default='Linf',
                         help="Attack Norm the selected model was trained with. Only necessary if you load robustbench models")
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     parser.add_argument('--attack_norm', type=int, default=1, choices=[1, 2, float('inf')],
                         help="Attack norm type (1, 2, float('inf'))")
     parser.add_argument('--max_iterations', type=int, default=500, help="Maximum iterations for attacks")
-    parser.add_argument('--max_batchsize', type=int, default=1, help="Maximum Batchsize to run every adversarial attack on." \
+    parser.add_argument('--max_batchsize', type=int, default=25, help="Maximum Batchsize to run every adversarial attack on." \
                         "If attack is not optimized or not working with batches, will be returned by attacks.AdversarialAttacks class.")
     parser.add_argument('--save_images', type=int, default=1, help="Integer > 0: number of saved images per attack, 0: do not save)")
     parser.add_argument('--verbose', type=utils.str2bool, nargs='?', const=False, default=False, help="Verbose output")
