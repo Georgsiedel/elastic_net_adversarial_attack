75405c076b5d0c1f6cef72c0e8d43077ac8055a8
Georgsiedel <georgsiedel@gmx.de>
2025-04-11 11:24:45 +0200

    results

--- run.py
+++ run.py
@@ -7,54 +7,13 @@ if __name__ == '__main__':
 
     import importlib
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1" #prevents "CUDA error: unspecified launch failure" and is recommended for some illegal memory access errors #increases train time by ~15%
-    
-    os.system("python hyperparameter_sweep.py --hyperparameter=learning_rate --hyperparameter_range 0.5 1.0 1.5 2.0 3.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --max_iterations=100 --attack_type=exp_attack_l1")
-    os.system("python hyperparameter_sweep.py --hyperparameter=beta --hyperparameter_range 1.0 2.0 3.0 5.0 10.0 20.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --max_iterations=100 --attack_type=exp_attack_l1")
 
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=100 --dataset=cifar10 --model=standard --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=100 --dataset=cifar10 --model=standard --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=100 --dataset=cifar10 --model=standard --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=100 --dataset=cifar10 --model=standard --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    #os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=100 --dataset=cifar10 --model=standard --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
 
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=300 --dataset=cifar10 --model=standard --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=300 --dataset=cifar10 --model=standard --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=300 --dataset=cifar10 --model=standard --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=300 --dataset=cifar10 --model=standard --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
- 
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=500 --dataset=cifar10 --model=standard --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=500 --dataset=cifar10 --model=standard --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=500 --dataset=cifar10 --model=standard --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=500 --dataset=cifar10 --model=standard --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=MainiAVG --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=MainiAVG --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=MainiAVG --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=MainiAVG --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --track_distance=True --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=MainiAVG --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
+    os.system("python attack_comparison.py --track_distance=True--max_batchsize=100 --beta=0.01 --learning_rate=1.0 --max_iterations=100 --dataset=cifar10 --model=standard --epsilon_l1=4.0 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
 
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=MainiAVG --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=MainiAVG --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=MainiAVG --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=MainiAVG --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")    
-    
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=MainiAVG --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=MainiAVG --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=MainiAVG --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=MainiAVG --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
-
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=CroceL1 --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=CroceL1 --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=CroceL1 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=CroceL1 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
-
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=CroceL1 --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=CroceL1 --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=CroceL1 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=300 --dataset=cifar10 --model=CroceL1 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")    
-    
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=CroceL1 --epsilon_l1=2 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=CroceL1 --epsilon_l1=4 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=CroceL1 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=500 --dataset=cifar10 --model=CroceL1 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --track_distance=True --max_batchsize=100 --beta=0.5 --learning_rate=2.0 --max_iterations=100 --dataset=cifar10 --model=CroceL1 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
 
     os.system("python attack_comparison.py --beta=2.0 --learning_rate=1.25 --max_iterations=100 --dataset=imagenet --model=standard --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
     os.system("python attack_comparison.py --beta=2.0 --learning_rate=1.25 --max_iterations=100 --dataset=imagenet --model=standard --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
@@ -85,6 +44,24 @@ if __name__ == '__main__':
     os.system("python attack_comparison.py --beta=5.0 --learning_rate=1.5 --max_iterations=500 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
     os.system("python attack_comparison.py --beta=5.0 --learning_rate=1.5 --max_iterations=500 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
     os.system("python attack_comparison.py --beta=5.0 --learning_rate=1.5 --max_iterations=500 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
+
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=100 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=100 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=100 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=100 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=100 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --attack_types exp_attack_l1 exp_attack_l1_linf exp_attack exp_attack_L1_rule_higher_beta")
+
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=300 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=300 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=300 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=300 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=300 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --attack_types exp_attack_l1 exp_attack_l1_linf")
+
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=500 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=500 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=500 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=500 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=2.0 --learning_rate=2.0 --max_iterations=500 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --attack_types exp_attack_l1 exp_attack_l1_linf")
 
     os.system("python attack_comparison.py --max_batchsize=25 --dataset=imagenet --model=ViT_revisiting --epsilon_l1=255 --max_iterations=300 --attack_types ead_fb ead_fb_L1_rule_higher_beta")
     os.system("python attack_comparison.py --max_batchsize=25 --dataset=imagenet --model=ViT_revisiting --epsilon_l1=255 --max_iterations=500")
