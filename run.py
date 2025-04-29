ebf65b8cbbb18af31ad7719ac8156a8e2ae238a4
Georgsiedel <georgsiedel@gmx.de>
2025-04-29 10:50:25 +0200

    run

--- run.py
+++ run.py
@@ -11,6 +11,21 @@ if __name__ == '__main__':
     os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 --dataset=imagenet --model=Salman2020Do_R50 --attack_type ead_fb --epsilon_l1=12 --max_iterations=300")
     os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 --dataset=imagenet --model=Salman2020Do_R50 --attack_type ead_fb_L1_rule_higher_beta --epsilon_l1=12 --max_iterations=300")
 
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 --dataset=imagenet --model=vgg19 --attack_type ead_fb --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 --dataset=imagenet --model=vgg19 --attack_type ead_fb_L1_rule_higher_beta --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 --dataset=imagenet --model=vgg19 --attack_type ead_fb --epsilon_l1=12 --max_iterations=300")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 --dataset=imagenet --model=vgg19 --attack_type ead_fb_L1_rule_higher_beta --epsilon_l1=12 --max_iterations=300")
+
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --track_c=True --track_distance=True --dataset=imagenet --model=vgg19 --epsilon_l1=12 --max_iterations=100 --attack_types exp_attack_l1 exp_attack_l1_linf ead_fb ead_fb_L1_rule_higher_beta")
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --dataset=imagenet --model=vgg19 --epsilon_l1=25 --max_iterations=100 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --dataset=imagenet --model=vgg19 --epsilon_l1=50 --max_iterations=100 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --dataset=imagenet --model=vgg19 --epsilon_l1=75 --max_iterations=100 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --track_c=True --track_distance=True --dataset=imagenet --model=vgg19 --epsilon_l1=12 --max_iterations=300 --attack_types exp_attack_l1 exp_attack_l1_linf ead_fb ead_fb_L1_rule_higher_beta")
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --dataset=imagenet --model=vgg19 --epsilon_l1=25 --max_iterations=300 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --dataset=imagenet --model=vgg19 --epsilon_l1=50 --max_iterations=300 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python attack_comparison.py --beta=0.05 --learning_rate=0.05 --dataset=imagenet --model=vgg19 --epsilon_l1=75 --max_iterations=300 --attack_types exp_attack_l1 exp_attack_l1_linf")
+
     os.system("python hyperparameter_sweep.py --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack_l1 --epsilon_l1=75 --max_iterations=100")
     os.system("python hyperparameter_sweep.py --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack_l1 --epsilon_l1=75 --max_iterations=100")
 
@@ -22,41 +37,27 @@ if __name__ == '__main__':
 
     os.system("python attack_comparison.py --verbose=True --max_iterations=30 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --samplesize_attack=100 --attack_types pointwise_blackbox square_l1_blackbox sparse_rs_custom_L1_blackbox geoda_blackbox")
   
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=cifar10 --model=MainiAVG --attack_type exp_attack_l1_blackbox --epsilon_l1=25 --max_iterations=300")
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=cifar10 --model=MainiAVG --attack_type exp_attack_l1_blackbox --epsilon_l1=25 --max_iterations=300")
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=cifar10 --model=MainiAVG --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=cifar10 --model=MainiAVG --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=MainiAVG --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=MainiAVG --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=cifar10 --model=standard --attack_type exp_attack_l1_blackbox --epsilon_l1=4 --max_iterations=300")
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=cifar10 --model=standard --attack_type exp_attack_l1_blackbox --epsilon_l1=4 --max_iterations=300")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=standard --attack_type exp_attack --epsilon_l1=2 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=standard --attack_type exp_attack --epsilon_l1=2 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=cifar10 --model=standard --attack_type exp_attack --epsilon_l1=2 --max_iterations=100")
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=cifar10 --model=standard --attack_type exp_attack --epsilon_l1=2 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=CroceL1 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=CroceL1 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=imagenet --model=standard --attack_type exp_attack_l1_blackbox --epsilon_l1=25 --max_iterations=300")
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=imagenet --model=standard --attack_type exp_attack_l1_blackbox --epsilon_l1=25 --max_iterations=300")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=corruption_robust --attack_type exp_attack --epsilon_l1=2 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=cifar10 --model=corruption_robust --attack_type exp_attack --epsilon_l1=2 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=imagenet --model=standard --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=imagenet --model=standard --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=standard --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=standard --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack_l1_blackbox --epsilon_l1=75 --max_iterations=300")
-    os.system("python hyperparameter_sweep.py --samplesize_attack=100 --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack_l1_blackbox --epsilon_l1=75 --max_iterations=300")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack --epsilon_l1=75 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack --epsilon_l1=75 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.1 0.25 1.0 2.5 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack --epsilon_l1=50 --max_iterations=100")
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.001 0.01 0.1 1.0 5.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack --epsilon_l1=50 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=Salman2020Do_R50 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=Salman2020Do_R50 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
 
-    os.system("python attack_comparison.py --track_distance=True --track_c=True --beta=7.0 --learning_rate=2.25 --max_iterations=100 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf ead_fb ead_fb_L1_rule_higher_beta")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=100 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=100 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=100 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
-
-    os.system("python attack_comparison.py --track_distance=True --track_c=True --beta=7.0 --learning_rate=2.25 --max_iterations=300 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf ead_fb ead_fb_L1_rule_higher_beta")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=300 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=300 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=300 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
-
-    os.system("python attack_comparison.py --track_distance=True --track_c=True --beta=7.0 --learning_rate=2.25 --max_iterations=500 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=12 --attack_types exp_attack_l1 exp_attack_l1_linf ead_fb ead_fb_L1_rule_higher_beta")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=500 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=25 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=500 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=50 --attack_types exp_attack_l1 exp_attack_l1_linf")
-    os.system("python attack_comparison.py --beta=7.0 --learning_rate=2.25 --max_iterations=500 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=75 --attack_types exp_attack_l1 exp_attack_l1_linf")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=learning_rate --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=DVCE_R50 --attack_type exp_attack --epsilon_l1=255 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter=beta --hyperparameter_range 0.02 0.05 0.1 0.2 0.5 1.0 2.0 --dataset=imagenet --model=DVCE_R50 --attack_type exp_attack --epsilon_l1=255 --max_iterations=100")
