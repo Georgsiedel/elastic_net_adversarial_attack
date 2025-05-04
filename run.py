1c8fac25b3d5d93c53cc6bf3e3ad042058311b87
Georgsiedel <georgsiedel@gmx.de>
2025-05-04 13:06:15 +0200

    res

--- run.py
+++ run.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     import importlib
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1" #prevents "CUDA error: unspecified launch failure" and is recommended for some illegal memory access errors #increases train time by ~15%
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.1 0.2 0.5 --hyperparameter_range2 0.5 1.0 --dataset=imagenet --model=standard --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.05 0.2 --hyperparameter_range2 0.1 0.2 --dataset=imagenet --model=standard --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.1 0.2 0.5 --hyperparameter_range2 0.2 0.5 1.0 --dataset=imagenet --model=vgg19 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.05 0.1 0.2 --hyperparameter_range2 0.1 0.2 0.5 --dataset=imagenet --model=vgg19 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
 
-    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.5 1.0 2.0 --hyperparameter_range2 1.0 2.0 5.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack --epsilon_l1=75 --max_iterations=100")
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 1.0 2.0 --hyperparameter_range2 1.0 2.0 5.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --attack_type exp_attack --epsilon_l1=75 --max_iterations=100")
 
     os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.5 1.0 2.0 --hyperparameter_range2 1.0 2.0 5.0 --dataset=imagenet --model=DVCE_R50 --attack_type exp_attack --epsilon_l1=255 --max_iterations=100")
 
