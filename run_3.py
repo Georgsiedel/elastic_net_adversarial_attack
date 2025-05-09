02b37fd4a1e3334939da7f805185b2d5021295ff
Georg Siedel <georgsiedel@gmx.de>
2025-05-09 14:33:07 +0000

    results, 4 runs.

---
+++ run_3.py
@@ -0,0 +1,42 @@
+import os
+import torch
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+if __name__ == '__main__':
+
+    import importlib
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1" #prevents "CUDA error: unspecified launch failure" and is recommended for some illegal memory access errors #increases train time by ~15%
+
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.2 0.5 1.0 --hyperparameter_range2 0.5 --dataset=imagenet --model=Salman2020Do_R50 --attack_type exp_attack --epsilon_l1=12 --max_iterations=100")
+
+    os.system("python hyperparameter_sweep.py --track_c=True --hyperparameter_range1 0.2 0.5 1.0 --hyperparameter_range2 0.5 1.0 2.0 --dataset=imagenet --model=Salman2020Do_R50 --attack_type exp_attack_L1_rule_higher_beta --epsilon_l1=12 --max_iterations=100")
+
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=12 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=12 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=25 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=25 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=50 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=50 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --max_iterations=300 --attack_types exp_attack_l1")
+
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=12 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=12 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=25 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=25 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=50 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=50 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=75 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=75 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=2.0 --beta=2.0 --dataset=imagenet --model=DVCE_R50 --epsilon_l1=255 --max_iterations=300 --attack_types exp_attack_l1")
+
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=12 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=12 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=25 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=25 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=50 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=50 --max_iterations=300 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=75 --max_iterations=100 --attack_types exp_attack_l1")
+    os.system("python attack_comparison.py --learning_rate=5.0 --beta=1.0 --dataset=imagenet --model=Salman2020Do_R50 --epsilon_l1=75 --max_iterations=300 --attack_types exp_attack_l1")
