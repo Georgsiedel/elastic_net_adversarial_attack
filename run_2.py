bbf0b9da14600caa80d52ed7b7691248a56c464d
Georg Siedel <georgsiedel@gmx.de>
2025-05-13 08:30:00 +0000

    results

--- run_2.py
+++ run_2.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     import importlib
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1" #prevents "CUDA error: unspecified launch failure" and is recommended for some illegal memory access errors #increases train time by ~15%
 
-    os.system("python attack_comparison.py --max_batchsize=200 --learning_rate=0.5 --beta=1.0 --track_distance=True --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=100 --max_iterations=100 --attack_types exp_attack")
+    os.system("python attack_comparison.py --learning_rate=0.5 --beta=1.0 --track_distance=True --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=100 --max_iterations=100 --attack_types exp_attack")
     #os.system("python attack_comparison.py --learning_rate=0.5 --beta=1.0 --track_distance=True --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=100 --max_iterations=300 --attack_types exp_attack")
 
-    os.system("python attack_comparison.py --max_batchsize=200 --learning_rate=0.2 --beta=2.0 --track_distance=True --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --max_iterations=100 --attack_types exp_attack_L1_rule_higher_beta")
+    os.system("python attack_comparison.py --learning_rate=0.2 --beta=2.0 --track_distance=True --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --max_iterations=100 --attack_types exp_attack_L1_rule_higher_beta")
     #os.system("python attack_comparison.py --learning_rate=0.2 --beta=2.0 --track_distance=True --dataset=imagenet --model=ConvNext_iso_CvSt_revisiting --epsilon_l1=75 --max_iterations=300 --attack_types exp_attack_L1_rule_higher_beta")
